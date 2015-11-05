c056143c5ce60d566bf88ce21c8acf7c1ebbadc3
Alban Linard <alban@linard.fr>
2015-11-05 16:39:21 +0100

    In ev module, only reload if computed hash differs. Fix #12

--- src/hotswap/ev.lua
+++ src/hotswap/ev.lua
@@ -1,5 +1,6 @@
 local posix   = require "posix"
 local ev      = require "ev"
+local xxhash  = require "xxhash"
 local Hotswap = getmetatable (require "hotswap")
 local Ev      = {}
 
@@ -9,6 +10,8 @@ function Ev.new (t)
     observe  = Ev.observe,
     loop     = t and t.loop or ev.Loop.default,
     observed = {},
+    hashes  = {},
+    seed    = 0x5bd1e995,
   }
 end
 
@@ -16,17 +19,28 @@ function Ev:observe (name, filename)
   if self.observed [name] then
     return
   end
+  do
+    local file = assert (io.open (filename, "r"))
+    self.hashes [name] = xxhash.xxh32 (file:read "*all", self.seed)
+    file:close ()
+  end
   local hotswap = self
   local current = filename
   repeat
     local stat = ev.Stat.new (function ()
-      hotswap.loaded [name] = nil
-      hotswap.try_require (name)
+      local file = assert (io.open (filename, "r"))
+      local hash = xxhash.xxh32 (file:read "*all", self.seed)
+      file:close ()
+      if hash ~= self.hashes [name] then
+        hotswap.loaded [name] = nil
+        hotswap.try_require (name)
+      end
     end, current)
     self.observed [current] = stat
     stat:start (hotswap.loop)
     current = posix.readlink (current)
   until not current
+
 end
 
 return Ev.new ()
